e40e030c4eccd65ad13282c4a35190a347d25ee7
qing zicheng <jeseeqing@gmail.com>
2023-03-30 16:10:06 +0800

    modify CreationAuditedEntity tags,add omitempty to json tag

--- audited_entity.go
+++ audited_entity.go
@@ -8,18 +8,18 @@ import (
 type CreationAuditedEntity struct {
 	Entity `bson:",inline"`
 	//create time
-	CreationTime time.Time `json:"creationTime" bson:"creationTime" `
+	CreationTime time.Time `json:"creationTime,omitempty" bson:"creationTime" `
 	//create user
-	CreatorId string `json:"creatorId" bson:"creatorId"`
+	CreatorId string `json:"creatorId,omitempty" bson:"creatorId"`
 }
 
 // auditable entity
 type AuditedEntity struct {
 	CreationAuditedEntity `bson:",inline"`
 	//last modification time
-	LastModificationTime *time.Time `json:"lastModificationTime" bson:"lastModificationTime"`
+	LastModificationTime *time.Time `json:"lastModificationTime,omitempty" bson:"lastModificationTime"`
 	//last modification user
-	LastModifierId string `json:"lastModifierId" bson:"lastModifierId"`
+	LastModifierId string `json:"lastModifierId,omitempty" bson:"lastModifierId"`
 }
 
 func (entity *CreationAuditedEntity) BeforeCreate() {
