8cc12d6c7687380dca4c8ddf165a29722edf2cba
qing zicheng <jeseeqing@gmail.com>
2023-04-21 18:33:18 +0800

    add FindAllT and FindT* function

---
+++ repository_t.go
@@ -0,0 +1,41 @@
+package mongodbr
+
+import (
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// find all t
+func FindAllT[T any](repository IRepository, opts ...FindOption) ([]T, error) {
+	res := repository.FindAll()
+	list := make([]T, 0)
+	if err := res.All(&list); err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+// find t by filter
+func FindTByFilter[T any](repository IRepository, filter interface{}, opts ...FindOption) ([]T, error) {
+	res := repository.FindByFilter(filter, opts...)
+	list := make([]T, 0)
+	if err := res.All(&list); err != nil {
+		return nil, err
+	}
+	return list, nil
+}
+
+// find t by _id
+func FindTByObjectId[T any](repository IRepository, id primitive.ObjectID) (*T, error) {
+	res := repository.FindByObjectId(id)
+	result := new(T)
+	if err := res.One(result); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return result, nil
+}
