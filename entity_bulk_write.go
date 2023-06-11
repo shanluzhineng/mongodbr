8c9508eae6a463957b2580885bd2c16ec4804459
zcqing <zcqing@wokanbit.com>
2023-06-11 15:52:14 +0800

    optimize IRepository BulkWrite func

--- entity_bulk_write.go
+++ entity_bulk_write.go
@@ -2,8 +2,11 @@ package mongodbr
 
 import (
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/abmpio/mongodbr/builder"
 )
 
 type EntityUpdate struct {
@@ -23,7 +26,37 @@ func _buildWriteModelForUpdate(list []IEntity) []mongo.WriteModel {
 	for _, eachEntity := range list {
 		currentModel := mongo.NewUpdateOneModel()
 		currentModel.SetFilter(bson.M{"_id": eachEntity.GetObjectId()})
-		currentModel.SetUpdate(eachEntity)
+		currentModel.SetUpdate(builder.NewBsonBuilder().NewOrUpdateSet(eachEntity))
+		modelList = append(modelList, currentModel)
+	}
+	return modelList
+}
+
+// build mongo.WriteModel list with ObjectId list
+func BuildWriteModelListWithObjectId(dataList map[primitive.ObjectID]interface{}) []mongo.WriteModel {
+	modelList := make([]mongo.WriteModel, 0)
+	if len(dataList) <= 0 {
+		return modelList
+	}
+	for eachObjectId, eachValue := range dataList {
+		currentModel := mongo.NewUpdateOneModel()
+		currentModel.SetFilter(bson.M{"_id": eachObjectId})
+		currentModel.SetUpdate(builder.NewBsonBuilder().NewOrUpdateSet(eachValue))
+		modelList = append(modelList, currentModel)
+	}
+	return modelList
+}
+
+// build mongo.WriteModel list with ObjectId list
+func BuildWriteModelList(filterList []interface{}, getUpdateFn func(filter interface{}) interface{}) []mongo.WriteModel {
+	modelList := make([]mongo.WriteModel, 0)
+	if len(filterList) <= 0 {
+		return modelList
+	}
+	for _, eachFilter := range filterList {
+		currentModel := mongo.NewUpdateOneModel()
+		currentModel.SetFilter(eachFilter)
+		currentModel.SetUpdate(builder.NewBsonBuilder().NewOrUpdateSet(getUpdateFn(eachFilter)))
 		modelList = append(modelList, currentModel)
 	}
 	return modelList
